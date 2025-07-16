7a395b529c2c33838419b07329007ce0b71a9a06
babal1111 <babaldon1111@gmail.com>
2025-07-16 11:53:19 +0530

    added socket io in server and roomController

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const roomRoutes = require('./src/routes/roomRoutes');
+const http = require('http');
+const {Server} = require('socket.io');
 
 app.use(express.json());
 
@@ -13,10 +15,39 @@ const CorsOptions= {
 };
 app.use(cors(CorsOptions));
 mongoose.connect(process.env.MONGODB_URL).then(()=>console.log('Connected to db')).catch((error)=> console.log(error));
+
+const server = http.createServer(app); //createserver is a handles function, app here acts as a handler
+// app , instance of express here will take care of the incomin req
+const io = new Server(server,{
+    cors:{
+        origin: process.env.CLIENT_URL,
+        methods:["GET","POST","DELETE","PUT"]
+    }
+})
+
+io.on("connection",(socket)=>{  // on connection event // and we recieve a socket object with http protocols,ids etc
+    console.log("New Client Connection :",socket.id);
+
+    socket.on("join-room",(roomCode)=>{ //ie when we recieve a event "join-room" well execute this
+        socket.join(roomCode);
+        console.log("Client Joined Room :",roomCode);
+    }) 
+    // it will  create a space, clients with same roomcode will be placed in the same pool(like an array of same data typre) and then if one sends msg all will get it
+
+    socket.on("disconnect",()=>{
+        console.log("Client Disconnected", socket.id);
+        // here we can add other features like, LAST SEEN(time of disconnect)
+    })
+
+    // these evens like join-room and disconnects will be emited by Client side
+}) 
+
+app.set("io",io); // by thi our server will be able to send all the connected clients, the msg on every new question
+
 app.use('/room',roomRoutes);
 
 const port = process.env.PORT;
-app.listen(port,(error)=>{
+server.listen(port,(error)=>{ 
     if(error) console.log(error);
     else console.log(`server is running at ${port}`);
 })
--- server/src/models/Rooms.js
+++ server/src/models/Rooms.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
     roomCode: {type: String,required: true,unique:true},
-    createBy:{type: String,},// this should reference to the user ubjectId
+    createdBy:{type: String,},// this should reference to the user ubjectId
     isActive:{type:Boolean, default: true},// like after 30 days of subscription change the is ACtivestaus
     createdAt:{type:Date,default: Date.now}
 });
